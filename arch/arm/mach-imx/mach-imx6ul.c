4a1174356e86cf4295de17580bf2ff11995c10e2
Zheng Bao <zheng.bao@amd.corp-partner.google.com>
2022-04-13 14:29:57 +0800

    nic. sd not 1.8v

--- arch/arm/mach-imx/mach-imx6ul.c
+++ arch/arm/mach-imx/mach-imx6ul.c
@@ -19,10 +19,14 @@
 #include <linux/regmap.h>
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
+#include <linux/memblock.h>
 
 #include "common.h"
 #include "cpuidle.h"
 #include "hardware.h"
+
+struct proc_dir_entry *boardname_dir, *boardname_file;  
+
 
 static void __init imx6ul_enet_clk_init(void)
 {
@@ -50,15 +54,65 @@ static int ksz8081_phy_fixup(struct phy_device *dev)
 	return 0;
 }
 
-#define PHY_ID_KSZ8081	0x00221560
+/*
+ * i.MX6UL EVK board RevA, RevB, RevC all use KSZ8081
+ * Silicon revision 00, the PHY ID is 0x00221560, pass our
+ * test with the phy fixup.
+ */
+#define PHY_ID_KSZ8081_MNRN60	0x00221560
+/*
+ * i.MX6UL EVK board RevC1 board use KSZ8081
+ * Silicon revision 01, the PHY ID is 0x00221561.
+ * This silicon revision still need the phy fixup setting.
+ */
+#define PHY_ID_KSZ8081_MNRN61	0x00221561
 static void __init imx6ul_enet_phy_init(void)
 {
-	phy_register_fixup_for_uid(PHY_ID_KSZ8081, 0xffffffff,	ksz8081_phy_fixup);
+	phy_register_fixup(PHY_ANY_ID, PHY_ID_KSZ8081_MNRN60, 0xffffffff, ksz8081_phy_fixup);
+	phy_register_fixup(PHY_ANY_ID, PHY_ID_KSZ8081_MNRN61, 0xffffffff, ksz8081_phy_fixup);
 }
+
+static int proc_show_ver(struct seq_file *file, void *v)
+{
+	int cnt = 0;
+  
+	if(of_machine_is_compatible("OKMX6UL-C"))
+		seq_printf(file, "boardname: %s\n", "OKMX6UL-C");    
+	else if(of_machine_is_compatible("OKMX6UL-C2"))
+		seq_printf(file, "boardname: %s\n", "OKMX6UL-C2");   
+	else if(of_machine_is_compatible("OKMX6UL-C3"))
+		seq_printf(file, "boardname: %s\n", "OKMX6UL-C3");  
+	
+	return cnt; 
+}
+static int proc_boardname_open(struct inode *inode, struct file *file)
+{
+	single_open(file, proc_show_ver, NULL); 
+	return 0;
+}
+
+static struct file_operations boardname_fops = {
+	.owner   = THIS_MODULE,
+	.open    = proc_boardname_open, 
+	.read    = seq_read,     
+	.llseek  = seq_lseek,
+	.release = single_release,  
+};
+
+int proc_init(void)
+{
+	boardname_file = proc_create("boardname", S_IFREG|S_IRUGO, NULL, &boardname_fops);
+   	if(boardname_file)
+   		return 0;
+	else
+   		return -ENOMEM;
+}
+
 
 #define OCOTP_CFG3			0x440
 #define OCOTP_CFG3_SPEED_SHIFT		16
 #define OCOTP_CFG3_SPEED_696MHZ		0x2
+#define OCOTP_CFG3_SPEED_1_GHZ		0x3
 
 static void __init imx6ul_opp_check_speed_grading(struct device *cpu_dev)
 {
@@ -66,7 +120,11 @@ static void __init imx6ul_opp_check_speed_grading(struct device *cpu_dev)
 	void __iomem *base;
 	u32 val;
 
-	np = of_find_compatible_node(NULL, NULL, "fsl,imx6ul-ocotp");
+	if (cpu_is_imx6ul())
+		np = of_find_compatible_node(NULL, NULL, "fsl,imx6ul-ocotp");
+	else
+		np = of_find_compatible_node(NULL, NULL, "fsl,imx6ull-ocotp");
+
 	if (!np) {
 		pr_warn("failed to find ocotp node\n");
 		return;
@@ -82,17 +140,30 @@ static void __init imx6ul_opp_check_speed_grading(struct device *cpu_dev)
 	 * Speed GRADING[1:0] defines the max speed of ARM:
 	 * 2b'00: Reserved;
 	 * 2b'01: 528000000Hz;
-	 * 2b'10: 700000000Hz;
-	 * 2b'11: Reserved;
+	 * 2b'10: 700000000Hz(i.MX6UL), 800000000Hz(i.MX6ULL);
+	 * 2b'11: Reserved(i.MX6UL), 1GHz(i.MX6ULL);
 	 * We need to set the max speed of ARM according to fuse map.
 	 */
 	val = readl_relaxed(base + OCOTP_CFG3);
 	val >>= OCOTP_CFG3_SPEED_SHIFT;
 	val &= 0x3;
+	if (cpu_is_imx6ul()) {
+		if (val < OCOTP_CFG3_SPEED_696MHZ) {
+			if (dev_pm_opp_disable(cpu_dev, 696000000))
+				pr_warn("Failed to disable 696MHz OPP\n");
+		}
+	}
 
-	if (val != OCOTP_CFG3_SPEED_696MHZ) {
-		if (dev_pm_opp_disable(cpu_dev, 696000000))
-			pr_warn("Failed to disable 696MHz OPP\n");
+	if (cpu_is_imx6ull()) {
+		if (val != OCOTP_CFG3_SPEED_1_GHZ) {
+			if (dev_pm_opp_disable(cpu_dev, 996000000))
+				pr_warn("Failed to disable 996MHz OPP\n");
+		}
+
+		if (val != OCOTP_CFG3_SPEED_696MHZ) {
+			if (dev_pm_opp_disable(cpu_dev, 792000000))
+				pr_warn("Failed to disable 792MHz OPP\n");
+		}
 	}
 	iounmap(base);
 
@@ -139,6 +210,8 @@ static inline void imx6ul_enet_init(void)
 static void __init imx6ul_init_machine(void)
 {
 	struct device *parent;
+	void __iomem *iomux;
+	struct device_node *np;	
 
 	parent = imx_soc_device_init();
 	if (parent == NULL)
@@ -148,6 +221,12 @@ static void __init imx6ul_init_machine(void)
 	imx6ul_enet_init();
 	imx_anatop_init();
 	imx6ul_pm_init();
+
+	np = of_find_compatible_node(NULL,NULL,"fsl,imx6ul-iomuxc");
+        iomux = of_iomap(np, 0);
+        writel_relaxed(0x2,iomux+0x650);
+        writel_relaxed(0x3,iomux+0x658);
+	proc_init();
 }
 
 static void __init imx6ul_init_irq(void)
@@ -161,8 +240,7 @@ static void __init imx6ul_init_irq(void)
 static void __init imx6ul_init_late(void)
 {
 	if (IS_ENABLED(CONFIG_ARM_IMX6Q_CPUFREQ)) {
-		if (cpu_is_imx6ul())
-			imx6ul_opp_init();
+		imx6ul_opp_init();
 		platform_device_register_simple("imx6q-cpufreq", -1, NULL, 0);
 	}
 
@@ -176,6 +254,30 @@ static void __init imx6ul_map_io(void)
 	imx_busfreq_map_io();
 }
 
+#ifdef CONFIG_MX6_CLK_FOR_BOOTUI_TRANS
+static void imx6ul_init_reserve(void)
+{
+	phys_addr_t base, size;
+
+	/*
+	 * Frame buffer base address.
+	 * It is same as CONFIG_FB_BASE in Uboot.
+	 */
+	base = 0x87b00000;
+
+	/*
+	 * Reserved display memory size.
+	 * It should be bigger than 3 x framer buffer size.
+	 * For 1080P 32 bpp, 1920*1080*4*3 = 0x017BB000.
+	 */
+	size = 0x01000000;
+
+	memblock_reserve(base, size);
+	memblock_remove(base, size);
+}
+#endif 
+
+
 static const char *imx6ul_dt_compat[] __initconst = {
 	"fsl,imx6ul",
 	"fsl,imx6ull",
@@ -188,4 +290,7 @@ DT_MACHINE_START(IMX6UL, "Freescale i.MX6 Ultralite (Device Tree)")
 	.init_machine	= imx6ul_init_machine,
 	.init_late	= imx6ul_init_late,
 	.dt_compat	= imx6ul_dt_compat,
+#ifdef CONFIG_MX6_CLK_FOR_BOOTUI_TRANS
+	.reserve        = imx6ul_init_reserve,
+#endif
 MACHINE_END
